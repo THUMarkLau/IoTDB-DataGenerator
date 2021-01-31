0ef032db9ca5253808b8cba0bc69879f4d2f5780
THUMarkLau <liuxuxin@outlook.com>
2021-01-31 13:33:29 +0800

    DataQuerier

--- src/main/java/DataCleaner.java
+++ src/main/java/DataCleaner.java
@@ -2,7 +2,7 @@ import org.apache.iotdb.session.*;
 
 
 public class DataCleaner {
-  private static final Session session = new Session("127.0.0.1", 6667, "root", "root");
+  private static final Session session = new Session("192.168.130.38", 6667, "root", "root");
 
   public static void main(String[] args) throws Exception {
     session.open(false);
---
+++ src/main/java/DataQuerier.java
@@ -0,0 +1,26 @@
+import org.apache.iotdb.session.*;
+
+public class DataQuerier {
+  private static final Session session = new Session("127.0.0.1", 6667, "root", "root");
+  private static final String[] sqls = new String[] {"select count(s0), count(s999) from root.test.device group by ([0, 100000), 100ms)"};
+  public static void main(String[] args) {
+    query();
+  }
+
+  static void query() {
+    for(String sql : sqls) {
+      try {
+        long startTime = System.currentTimeMillis();
+        SessionDataSet dataset = session.executeQueryStatement(sql);
+        dataset.setFetchSize(1024);
+        while(dataset.hasNext()) {
+          dataset.next();
+        }
+        long lastingTime = System.currentTimeMillis() - startTime;
+        System.out.println("SQL:" + sql + ": " + lastingTime + " ms");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
