e4fe9ce7e12f71ba2b84aa03a65f184870694ba2
THUMarkLau <liuxuxin@outlook.com>
2021-01-31 13:52:47 +0800

    add loop

--- src/main/java/DataQuerier.java
+++ src/main/java/DataQuerier.java
@@ -7,24 +7,28 @@ public class DataQuerier {
   private static final String[] sqls = new String[] {"select count(s0), count(s999) from root.test.device group by ([0, 100000), 100ms)"};
   public static void main(String[] args) throws IoTDBConnectionException, StatementExecutionException {
     session.open(false);
-    session.executeNonQueryStatement("clear cache");
+
     query();
   }
 
   static void query() {
     for(String sql : sqls) {
-      try {
-        long startTime = System.currentTimeMillis();
-
-        SessionDataSet dataset = session.executeQueryStatement(sql);
-        dataset.setFetchSize(1024);
-        while(dataset.hasNext()) {
-          dataset.next();
+      long totalTime = 0;
+      for (int i = 0; i < 100; i++) {
+        try {
+          session.executeNonQueryStatement("clear cache");
+          long startTime = System.currentTimeMillis();
+          SessionDataSet dataset = session.executeQueryStatement(sql);
+          dataset.setFetchSize(1024);
+          while (dataset.hasNext()) {
+            dataset.next();
+          }
+          long lastingTime = System.currentTimeMillis() - startTime;
+          totalTime += lastingTime;
+        } catch (Exception e) {
+          e.printStackTrace();
         }
-        long lastingTime = System.currentTimeMillis() - startTime;
-        System.out.println("SQL:" + sql + ": " + lastingTime + " ms");
-      } catch (Exception e) {
-        e.printStackTrace();
+        System.out.println("SQL:" + sql + ": " + totalTime / 100l + " ms");
       }
     }
   }
