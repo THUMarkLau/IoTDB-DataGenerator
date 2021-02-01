2e5f84205355a7ac3a96bdeb2d45d7b25314ca2a
THUMarkLau <liuxuxin@outlook.com>
2021-02-01 16:51:22 +0800

    csv read

---
+++ src/main/java/CostEstimator.java
@@ -0,0 +1,65 @@
+import com.csvreader.CsvReader;
+import org.apache.iotdb.tsfile.utils.Pair;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CostEstimator {
+  private static List<Pair<Long, Float>> empiricalData = new ArrayList<>();
+  private static boolean init = false;
+  static boolean readEmpiricalFile() {
+    final File EMPIRICAL_SEEK_FILE = new File("/data/iotdb/empiricalData/seek_time.csv");
+    try{
+      if (!EMPIRICAL_SEEK_FILE.exists()) {
+        return false;
+      }
+      CsvReader csvReader = new CsvReader(EMPIRICAL_SEEK_FILE.getAbsolutePath());
+      csvReader.readHeaders();
+      while(csvReader.readRecord()) {
+        String blockSize = csvReader.get("BlockSize");
+        String averageTime = csvReader.get("AverageTime");
+        empiricalData.add(new Pair<Long, Float>(stringDataToBytes(blockSize), Float.valueOf(averageTime)));
+      }
+      csvReader.close();
+      return true;
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  static long stringDataToBytes(String data) {
+    String[] suffixes = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+    long result = 0;
+    for(int i = suffixes.length - 1; i >= 0; --i) {
+      if (data.endsWith(suffixes[i])) {
+        int pos = data.indexOf(suffixes[i]);
+        float base = Float.valueOf(data.substring(0, pos));
+        for(int j = 0; j < i; ++j) {
+          base *= 1024.0;
+        }
+        result = (long)base;
+        break;
+      }
+    }
+    return result;
+  }
+
+  static float getSeekCost(long distance) {
+    if (!init) {
+      readEmpiricalFile();
+    }
+    float seekCost = 0;
+    for(int i = 0; i < empiricalData.size() - 1; ++i) {
+      if (distance >= empiricalData.get(i).left && distance < empiricalData.get(i + 1).left) {
+        seekCost = (float)(distance - empiricalData.get(i).left) / (float)(empiricalData.get(i+1).left - empiricalData.get(i).left);
+        seekCost = seekCost * (empiricalData.get(i+1).right - empiricalData.get(i).right) + empiricalData.get(i).right;
+        break;
+      }
+    }
+    return seekCost;
+  }
+}
--- src/main/java/TsFileReadTime.java
+++ src/main/java/TsFileReadTime.java
@@ -10,25 +10,50 @@ import org.apache.iotdb.tsfile.read.expression.impl.SingleSeriesExpression;
 import org.apache.iotdb.tsfile.read.filter.TimeFilter;
 import org.apache.iotdb.tsfile.read.query.dataset.QueryDataSet;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class TsFileReadTime {
-  static String filePath = "/data/iotdb/sequence/root.test/0/....tsfile";
+  static String filePath = "/data/iotdb/sequence/root.test/0/1612026727214-1-0.tsfile";
   public static void main(String[] args) throws Exception{
+    int dataCount = 100;
+    if (args.length > 0) {
+      dataCount = Integer.valueOf(args[0]);
+    }
     try(TsFileSequenceReader reader = new TsFileSequenceReader(filePath);
         ReadOnlyTsFile readOnlyTsFile = new ReadOnlyTsFile(reader)) {
       ArrayList<Path> paths = new ArrayList<>();
-      paths.add(new Path("root.test.device", "s0"));
-      paths.add(new Path("root.test.device", "s999"));
-      IExpression timeFilter = new GlobalTimeExpression(TimeFilter.ltEq(10000l));
-      QueryExpression queryExpression = QueryExpression.create(paths, timeFilter);
+      Set<Integer> idxSet = new HashSet<>();
+      Random r = new Random();
+      while (idxSet.size() < dataCount) {
+        int value = r.nextInt() % 1000;
+        value = value < 0 ? -value : value;
+        idxSet.add(value);
+      }
+      List<Integer> measurementIdx = new ArrayList<>();
+      for(Integer idx : idxSet) {
+        measurementIdx.add(idx);
+      }
+      Collections.sort(measurementIdx);
+      float estimatedCost = 0;
+      for(int i = 0; i < measurementIdx.size() - 1; ++i) {
+        if (measurementIdx.get(i+1) - measurementIdx.get(i) > 1) {
+          estimatedCost += CostEstimator.getSeekCost((long)(measurementIdx.get(i+1) - measurementIdx.get(i) - 1) * 80643l);
+        }
+      }
+      System.out.println(measurementIdx.toString());
+      for(Integer idx : measurementIdx) {
+        paths.add(new Path("root.test.device", "s" + idx));
+      }
+      // IExpression timeFilter = new GlobalTimeExpression(TimeFilter.ltEq(10000l));
+      QueryExpression queryExpression = QueryExpression.create(paths, null);
       QueryDataSet dataSet = readOnlyTsFile.query(queryExpression);
       long startTime = System.currentTimeMillis();
       while(dataSet.hasNext()) {
         dataSet.next();
       }
       long lastTime = System.currentTimeMillis() - startTime;
-      System.out.println("Query time: " + lastTime + " ms");
+      System.out.println("Estimation seek time: " + estimatedCost + "ms");
+      System.out.println("Real query time: " + lastTime + " ms");
     }
   }
 }
