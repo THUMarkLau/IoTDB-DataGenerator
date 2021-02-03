13879e87be2a2c295f647235091ff203f4c859a2
THUMarkLau <liuxuxin@outlook.com>
2021-02-03 14:49:32 +0800

    data querier:

--- src/main/java/DataQuerier.java
+++ src/main/java/DataQuerier.java
@@ -2,34 +2,58 @@ import org.apache.iotdb.rpc.IoTDBConnectionException;
 import org.apache.iotdb.rpc.StatementExecutionException;
 import org.apache.iotdb.session.*;
 
+import java.util.*;
+
 public class DataQuerier {
   private static final Session session = new Session("127.0.0.1", 6667, "root", "root");
-  private static final String[] sqls = new String[] {"select count(s0), count(s999) from root.test.device group by ([0, 100000), 100ms)"};
+  static String GROUP_BY_ARGS = " group by ([0, 100000), 25000ms)";
   public static void main(String[] args) throws IoTDBConnectionException, StatementExecutionException {
     session.open(false);
-
-    query();
-  }
-
-  static void query() {
-    for(String sql : sqls) {
-      long totalTime = 0;
-      for (int i = 0; i < 100; i++) {
-        try {
-          session.executeNonQueryStatement("clear cache");
-          long startTime = System.currentTimeMillis();
-          SessionDataSet dataset = session.executeQueryStatement(sql);
-          dataset.setFetchSize(1024);
-          while (dataset.hasNext()) {
-            dataset.next();
-          }
-          long lastingTime = System.currentTimeMillis() - startTime;
-          totalTime += lastingTime;
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-        System.out.println("SQL:" + sql + "(average): " + totalTime / 100l + " ms");
+    int dataCount = 100;
+    if (args.length > 0) {
+      dataCount = Integer.valueOf(args[0]);
+    }
+    Set<Integer> idxSet = new HashSet<>();
+    Random r = new Random();
+    while(idxSet.size() < dataCount) {
+      int value = r.nextInt() % 1000;
+      value = value < 0 ? -value : value;
+      idxSet.add(value);
+    }
+    List<Integer> measurementIdx = new ArrayList<>();
+    for(Integer idx : idxSet) {
+      measurementIdx.add(idx);
+    }
+    Collections.sort(measurementIdx);
+    StringBuilder sql = new StringBuilder();
+    sql.append("Select ");
+    for(int i = 0; i < measurementIdx.size(); ++i) {
+      sql.append("avg(s" + i + ")");
+      if (i != measurementIdx.size() - 1) {
+        sql.append(", ");
+      } else {
+        sql.append(" ");
       }
     }
+    sql.append("from root.test.device");
+    sql.append(GROUP_BY_ARGS);
+    System.out.println("Executing sql: " + sql.toString());
+    session.executeNonQueryStatement("clear cache");
+    long startTime = System.currentTimeMillis();
+    SessionDataSet dataSet = session.executeQueryStatement(sql.toString());
+    while(dataSet.hasNext()) {
+      dataSet.next();
+    }
+    long lastTime = System.currentTimeMillis() - startTime;
+    float estimatedCost = 0;
+    for(int i = 0; i < measurementIdx.size() - 1; ++i) {
+      if (measurementIdx.get(i+1) - measurementIdx.get(i) > 1) {
+        estimatedCost += CostEstimator.getSeekCost((long)(measurementIdx.get(i+1) - measurementIdx.get(i) - 1) * 161178l);
+      }
+    }
+    System.out.println("Estimated seek time: " + (estimatedCost * 5.0f) + " ms");
+    System.out.println("Estimated read time: " + ((161178.0f / (80645.0 / 1.1)) * (dataCount * 5)) + " ms");
+    System.out.println("Estimated total time: " + (estimatedCost * 5.0f + 6 + 161178.0f / (80645.0 / 1.1) * dataCount * 5) + "ms");
+    System.out.println("Real query time: " + lastTime + " ms");
   }
 }
--- src/main/java/TsFileReadTime.java
+++ src/main/java/TsFileReadTime.java
@@ -1,4 +1,5 @@
 import org.apache.iotdb.tsfile.*;
+import org.apache.iotdb.tsfile.file.metadata.TimeseriesMetadata;
 import org.apache.iotdb.tsfile.read.ReadOnlyTsFile;
 import org.apache.iotdb.tsfile.read.TsFileSequenceReader;
 import org.apache.iotdb.tsfile.read.common.Path;
@@ -13,7 +14,7 @@ import org.apache.iotdb.tsfile.read.query.dataset.QueryDataSet;
 import java.util.*;
 
 public class TsFileReadTime {
-  static String filePath = "/data/iotdb/sequence/root.test/0/1612026727214-1-0.tsfile";
+  static String filePath = "/data/iotdb/sequence/root.test/0/1612249694311-1-0.tsfile";
   public static void main(String[] args) throws Exception{
     int dataCount = 100;
     if (args.length > 0) {
@@ -37,7 +38,7 @@ public class TsFileReadTime {
       float estimatedCost = 0;
       for(int i = 0; i < measurementIdx.size() - 1; ++i) {
         if (measurementIdx.get(i+1) - measurementIdx.get(i) > 1) {
-          estimatedCost += CostEstimator.getSeekCost((long)(measurementIdx.get(i+1) - measurementIdx.get(i) - 1) * 80643l);
+          estimatedCost += CostEstimator.getSeekCost((long)(measurementIdx.get(i+1) - measurementIdx.get(i) - 1) * 161178l);
         }
       }
       System.out.println(measurementIdx.toString());
@@ -48,7 +49,10 @@ public class TsFileReadTime {
       }
 
       long startTime = System.currentTimeMillis();
-      reader.readTimeseriesMetadata("root.test.device", sensorSet);
+      List<TimeseriesMetadata> metadatas = reader.readTimeseriesMetadata("root.test.device", sensorSet);
+      for(TimeseriesMetadata metadata : metadatas) {
+
+      }
       long metadataReadTime = System.currentTimeMillis() - startTime;
       System.out.println("Metadata read time: " + metadataReadTime + " ms");
       // IExpression timeFilter = new GlobalTimeExpression(TimeFilter.ltEq(10000l));
@@ -59,7 +63,9 @@ public class TsFileReadTime {
         dataSet.next();
       }
       long lastTime = System.currentTimeMillis() - startTime;
-      System.out.println("Estimation seek time: " + estimatedCost * 10.0f + "ms");
+      System.out.println("Estimated seek time: " + (estimatedCost * 5.0f) + " ms");
+      System.out.println("Estimated read time: " + ((161178.0f / (80645.0 / 1.1)) * (dataCount * 5)) + " ms");
+      System.out.println("Estimated total time: " + (estimatedCost * 5.0f + 6 + 161178.0f / (80645.0 / 1.1) * dataCount * 5) + "ms");
       System.out.println("Real query time: " + lastTime + " ms");
     }
   }
